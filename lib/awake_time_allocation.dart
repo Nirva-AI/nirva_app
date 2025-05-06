75068e485c2aaff8d755dfffd4da32e5371c7ff4
yanghanggit <1220826563@qq.com>
2025-05-06 16:47:53 +0800

    AwakeTimeAllocation 第一次调整基本达标，准备进一步细化。

--- lib/awake_time_allocation.dart
+++ lib/awake_time_allocation.dart
@@ -20,6 +20,63 @@ class AwakeTimeAllocation extends StatelessWidget {
               height: 200,
               child: BarChart(
                 BarChartData(
+                  alignment: BarChartAlignment.spaceAround,
+                  maxY: 8, // 设置纵轴最大值为 8
+                  titlesData: FlTitlesData(
+                    leftTitles: AxisTitles(
+                      sideTitles: SideTitles(
+                        showTitles: true,
+                        reservedSize: 28,
+                        getTitlesWidget: (value, meta) {
+                          return Text(
+                            '${value.toInt()}h',
+                            style: const TextStyle(fontSize: 12),
+                          );
+                        },
+                      ),
+                    ),
+                    bottomTitles: AxisTitles(
+                      sideTitles: SideTitles(
+                        showTitles: true,
+                        getTitlesWidget: (value, meta) {
+                          switch (value.toInt()) {
+                            case 0:
+                              return const Text(
+                                'Work',
+                                style: TextStyle(fontSize: 12),
+                              );
+                            case 1:
+                              return const Text(
+                                'Exercise',
+                                style: TextStyle(fontSize: 12),
+                              );
+                            case 2:
+                              return const Text(
+                                'Social',
+                                style: TextStyle(fontSize: 12),
+                              );
+                            case 3:
+                              return const Text(
+                                'Learning',
+                                style: TextStyle(fontSize: 12),
+                              );
+                            case 4:
+                              return const Text(
+                                'Self-care',
+                                style: TextStyle(fontSize: 12),
+                              );
+                            case 5:
+                              return const Text(
+                                'Other',
+                                style: TextStyle(fontSize: 12),
+                              );
+                            default:
+                              return const Text('');
+                          }
+                        },
+                      ),
+                    ),
+                  ),
                   barGroups: [
                     BarChartGroupData(
                       x: 0,
@@ -33,7 +90,23 @@ class AwakeTimeAllocation extends StatelessWidget {
                       x: 2,
                       barRods: [BarChartRodData(toY: 3, color: Colors.red)],
                     ),
+                    BarChartGroupData(
+                      x: 3,
+                      barRods: [BarChartRodData(toY: 3, color: Colors.orange)],
+                    ),
+                    BarChartGroupData(
+                      x: 4,
+                      barRods: [BarChartRodData(toY: 1, color: Colors.yellow)],
+                    ),
+                    BarChartGroupData(
+                      x: 5,
+                      barRods: [
+                        BarChartRodData(toY: 4, color: Colors.blueGrey),
+                      ],
+                    ),
                   ],
+                  gridData: FlGridData(show: false),
+                  borderData: FlBorderData(show: false),
                 ),
               ),
             ),
