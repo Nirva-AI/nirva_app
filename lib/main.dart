8d782a62aa5242b19cb27bb685254482a24b1501
yanghanggit <1220826563@qq.com>
2025-05-08 16:47:07 +0800

    数据提取。

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:nirva_app/main_app.dart';
 import 'package:nirva_app/data_manager.dart';
 import 'package:nirva_app/chat_manager.dart';
-import 'package:nirva_app/utils.dart';
+//import 'package:nirva_app/utils.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // 确保初始化完成
 
   // 使用 Utils 类的静态方法读取并解析 JSON 文件
-  final Map<String, dynamic> jsonData = await Utils.loadJsonAsset(
-    'assets/test.json',
-  );
-  debugPrint(jsonData.toString()); // 打印解析后的 Map 数据
+  // final Map<String, dynamic> jsonData = await Utils.loadJsonAsset(
+  //   'assets/test.json',
+  // );
+  // debugPrint(jsonData.toString()); // 打印解析后的 Map 数据
 
   // 初始化其他模块
   DataManager().initialize();
+  await DataManager().loadTestData();
   ChatManager().initialize();
 
   // 运行应用
