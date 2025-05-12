8004ba6beb8ba2b4e1705c8f3b31a873dc08e0df
yanghanggit <1220826563@qq.com>
2025-05-12 18:50:03 +0800

    一些进展，保证手机真机 + 模拟器都可以访问局域网。待进一步开发。

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:nirva_app/main_app.dart';
 import 'package:nirva_app/data_manager.dart';
 import 'package:nirva_app/chat_manager.dart';
+import 'package:nirva_app/service_manager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // 确保初始化完成
+
+  await ServiceManager().postAPIEndPointConfiguration();
 
   // 初始化其他模块
   DataManager().initialize();
--- lib/me_page.dart
+++ lib/me_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:nirva_app/service_manager.dart';
-import 'dart:convert';
 
 class MePage extends StatefulWidget {
   const MePage({super.key});
@@ -10,39 +9,19 @@ class MePage extends StatefulWidget {
 }
 
 class _MePageState extends State<MePage> {
-  late Future<String> _apiEndpointsFuture;
-
-  @override
-  void initState() {
-    super.initState();
-    _apiEndpointsFuture = _fetchApiEndpoints();
-  }
-
-  Future<String> _fetchApiEndpoints() async {
-    await ServiceManager().postAPIEndPointConfiguration();
-    return jsonEncode(ServiceManager().api_endpoints.toJson());
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('User Profile Screen')),
-      body: FutureBuilder<String>(
-        future: _apiEndpointsFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else {
-            return Center(
-              child: Text(
-                snapshot.data ?? 'No data available',
-                textAlign: TextAlign.center,
-              ),
-            );
-          }
-        },
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'User Profile Screen = ${ServiceManager().apiEndPointsJson()}',
+              style: TextStyle(fontSize: 12),
+            ),
+          ],
+        ),
       ),
     );
   }
