b82c127123f6ff85fd4847b164cbdffb035ddf6f
yanghanggit <1220826563@qq.com>
2025-05-12 16:32:04 +0800

    测试引入dio库。模拟器测试完成。

---
+++ lib/api.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: non_constant_identifier_names
+// 请注意，本页是为了对接python fastapi服务器的数据定义的代码，命名风格完全和服务器一致，所以关警告。不要修改命名风格。
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'api.freezed.dart';
+part 'api.g.dart';
+
+@freezed
+class User with _$User {
+  const factory User({required int id, required String name}) = _User;
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+
+  @override
+  Map<String, dynamic> toJson() => (this as _User).toJson();
+}
+
+@freezed
+class APIEndpointConfiguration with _$APIEndpointConfiguration {
+  const factory APIEndpointConfiguration({
+    required String LOGIN_URL,
+    required String LOGOUT_URL,
+    required String CHAT_ACTION_URL,
+  }) = _APIEndpointConfiguration;
+
+  factory APIEndpointConfiguration.fromJson(Map<String, dynamic> json) =>
+      _$APIEndpointConfigurationFromJson(json);
+  @override
+  Map<String, dynamic> toJson() => (this as _APIEndpointConfiguration).toJson();
+}
+
+@freezed
+class APIEndpointConfigurationResponse with _$APIEndpointConfigurationResponse {
+  const factory APIEndpointConfigurationResponse({
+    required APIEndpointConfiguration api_endpoints,
+    required int error,
+    required String message,
+  }) = _APIEndpointConfigurationResponse;
+
+  factory APIEndpointConfigurationResponse.fromJson(
+    Map<String, dynamic> json,
+  ) => _$APIEndpointConfigurationResponseFromJson(json);
+  @override
+  Map<String, dynamic> toJson() =>
+      (this as _APIEndpointConfigurationResponse).toJson();
+}
