7d52a97c0a1d3edcc3c74c37642a66f36d812395
yanghanggit <1220826563@qq.com>
2025-05-13 14:37:03 +0800

    临时添加获取网络状态。

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:nirva_app/home_page.dart';
 import 'package:nirva_app/service_manager.dart';
 import 'package:nirva_app/data_manager.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -22,6 +23,9 @@ class _SplashScreenState extends State<SplashScreen> {
   Future<void> _initialize() async {
     final int minDuration = 400; // 最小等待时间
     final Stopwatch stopwatch = Stopwatch()..start();
+
+    // 检查网络状态，临时先这么写。
+    await checkNetworkStatus();
 
     // 调用 configureApiEndpoint 并等待返回
     final bool apiConfigurationSuccess =
@@ -84,5 +88,35 @@ class _SplashScreenState extends State<SplashScreen> {
         ),
       ),
     );
+  }
+
+  // 网络状态检查失败后的处理: 如果用户没有网络连接，是否需要阻止后续逻辑执行？目前代码中即使没有网络连接，仍会继续执行后续的 API 配置和登录逻辑。
+  // 用户体验优化: 在网络状态检查失败时，可以提供更多选项（如重试按钮）而不是仅仅显示一个确认对话框。
+  Future<void> checkNetworkStatus() async {
+    final connectivityResult = await Connectivity().checkConnectivity();
+    if (connectivityResult == ConnectivityResult.none) {
+      if (!mounted) return;
+
+      await showDialog(
+        context: context,
+        builder:
+            (context) => AlertDialog(
+              title: const Text("No Network Connection"),
+              content: const Text(
+                "Please check your network connection and try again.",
+              ),
+              actions: [
+                TextButton(
+                  onPressed:
+                      () => {
+                        debugPrint("No Network Connection"),
+                        Navigator.pop(context),
+                      },
+                  child: const Text("OK"),
+                ),
+              ],
+            ),
+      );
+    }
   }
 }
