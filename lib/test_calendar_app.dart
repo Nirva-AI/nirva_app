1d367f4282c6cd86f0a1155d01630c68a1828cca
yanghanggit <1220826563@qq.com>
2025-05-20 22:29:20 +0800

    添加 table_calendar，并测试。

--- lib/test_calendar_app.dart
+++ lib/test_calendar_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class TestCalendarApp extends StatelessWidget {
   const TestCalendarApp({super.key});
@@ -23,8 +24,42 @@ class TestCalendarView extends StatefulWidget {
 }
 
 class _TestCalendarViewState extends State<TestCalendarView> {
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('测试日历!')));
+    return Scaffold(
+      appBar: AppBar(title: const Text('测试日历!')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: TableCalendar(
+          firstDay: DateTime.utc(2000, 1, 1),
+          lastDay: DateTime.utc(2100, 12, 31),
+          focusedDay: _focusedDay,
+          selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+          onDaySelected: (selectedDay, focusedDay) {
+            setState(() {
+              _selectedDay = selectedDay;
+              _focusedDay = focusedDay;
+            });
+          },
+          calendarStyle: const CalendarStyle(
+            todayDecoration: BoxDecoration(
+              color: Colors.blue,
+              shape: BoxShape.circle,
+            ),
+            selectedDecoration: BoxDecoration(
+              color: Colors.orange,
+              shape: BoxShape.circle,
+            ),
+          ),
+          headerStyle: const HeaderStyle(
+            formatButtonVisible: false,
+            titleCentered: true,
+          ),
+        ),
+      ),
+    );
   }
 }
