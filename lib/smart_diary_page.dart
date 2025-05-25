972bc041d30c1fce0f6700cb975b39187a2195d1
yanghanggit <1220826563@qq.com>
2025-05-25 22:40:10 +0800

    添加日历组件到smart diary。

--- lib/smart_diary_page.dart
+++ lib/smart_diary_page.dart
@@ -2,10 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:nirva_app/diary_entry_card.dart';
 import 'package:nirva_app/data_manager.dart';
 import 'package:nirva_app/quote_carousel.dart';
-import 'package:nirva_app/utils.dart';
+import 'package:nirva_app/week_calendar_widget.dart';
 
-class SmartDiaryPage extends StatelessWidget {
+class SmartDiaryPage extends StatefulWidget {
   const SmartDiaryPage({super.key});
+
+  @override
+  State<SmartDiaryPage> createState() => _SmartDiaryPageState();
+}
+
+class _SmartDiaryPageState extends State<SmartDiaryPage> {
+  late DateTime _focusedDay;
+  DateTime? _selectedDay;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusedDay = DataManager().currentJournal.dateTime;
+    _selectedDay = DataManager().currentJournal.dateTime;
+  }
+
+  void _updateSelectedDay(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      _selectedDay = selectedDay;
+      _focusedDay = focusedDay;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,17 +40,17 @@ class SmartDiaryPage extends StatelessWidget {
             padding: const EdgeInsets.all(16.0),
             child: QuoteCarousel(quotes: DataManager().currentJournal.quotes),
           ),
-          // 日期标题
+
+          // 周日历组件替代原来的日期标题
           Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 16.0,
-              vertical: 8.0,
-            ),
-            child: Text(
-              Utils.fullDiaryDateTime(DataManager().currentJournal.dateTime),
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: WeekCalendarWidget(
+              focusedDay: _focusedDay,
+              selectedDay: _selectedDay,
+              onDaySelected: _updateSelectedDay,
             ),
           ),
+
           // 使用 ListView.builder 动态展示日记条目
           ListView.builder(
             shrinkWrap: true, // 使 ListView 适应父组件高度
