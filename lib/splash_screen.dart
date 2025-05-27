ae06aa22ebf83df065a537f368686fdfd6140cae
yanghanggit <1220826563@qq.com>
2025-05-27 14:33:39 +0800

    打开，后续需要测试。

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:nirva_app/home_page.dart';
-//import 'package:nirva_app/service_manager.dart';
+import 'package:nirva_app/service_manager.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:nirva_app/data_manager.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -11,7 +12,7 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  //final ServiceManager _serviceManager = ServiceManager();
+  final ServiceManager _serviceManager = ServiceManager();
 
   @override
   void initState() {
@@ -23,22 +24,22 @@ class _SplashScreenState extends State<SplashScreen> {
     final int minDuration = 400; // 最小等待时间
     final Stopwatch stopwatch = Stopwatch()..start();
 
-    // 检查网络状态，临时先这么写。
-    // await checkNetworkStatus();
+    //检查网络状态，临时先这么写。
+    await checkNetworkStatus();
 
-    // // 调用 configureApiEndpoint 并等待返回
-    // final bool apiConfigurationSuccess = await _serviceManager.get_url_config();
+    // 调用 configureApiEndpoint 并等待返回
+    final bool apiConfigurationSuccess = await _serviceManager.getUrlConfig();
 
-    // //
-    // if (apiConfigurationSuccess) {
-    //   debugPrint('API Endpoint Configuration: $apiConfigurationSuccess');
+    //
+    if (apiConfigurationSuccess) {
+      debugPrint('API Endpoint Configuration: $apiConfigurationSuccess');
 
-    //   // 如果配置成功，调用 login 方法
-    //   final bool loginSuccess = await _serviceManager.login(
-    //     DataManager().userName,
-    //   );
-    //   debugPrint('Login: $loginSuccess');
-    // }
+      // 如果配置成功，调用 login 方法
+      final bool loginSuccess = await _serviceManager.login(
+        DataManager().user.name,
+      );
+      debugPrint('Login: $loginSuccess');
+    }
 
     // 计算剩余时间
     final int elapsed = stopwatch.elapsedMilliseconds;
