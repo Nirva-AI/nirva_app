0b9bf54be64854459acb1fbbaa5d9b1a9731ae87
yanghanggit <1220826563@qq.com>
2025-06-04 21:44:24 +0800

    调整moodscore 与 stresslevel card

--- lib/stress_level_card.dart
+++ lib/stress_level_card.dart
@@ -1,27 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:nirva_app/app_runtime_context.dart';
 import 'package:nirva_app/data.dart';
 import 'package:nirva_app/stress_level_details_page.dart'; // 导入新页面
 
 class StressLevelCard extends StatelessWidget {
-  final StressLevel data;
-
-  const StressLevelCard({super.key, required this.data});
-
-  // 根据变化值获取颜色
-  Color _getChangeColor(double change) {
-    return change >= 0 ? Colors.green : Colors.red;
-  }
-
-  // 根据变化值返回格式化字符串
-  String _formatChange(double change) {
-    return change >= 0
-        ? '+${change.toStringAsFixed(1)}'
-        : change.toStringAsFixed(1);
-  }
+  const StressLevelCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final Color changeColor = _getChangeColor(data.change);
+    final stressLevel =
+        AppRuntimeContext().data.currentJournalFile.stressLevelAverage;
+
     return Expanded(
       child: Card(
         elevation: 2,
@@ -50,27 +39,11 @@ class StressLevelCard extends StatelessWidget {
               ),
               const SizedBox(height: 8),
               Text(
-                data.value.toStringAsFixed(1),
+                stressLevel.toStringAsFixed(1),
                 style: const TextStyle(
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
                 ),
-              ),
-              const SizedBox(height: 8),
-              Row(
-                children: [
-                  Icon(
-                    changeColor == Colors.green
-                        ? Icons.arrow_upward
-                        : Icons.arrow_downward,
-                    color: changeColor,
-                    size: 16,
-                  ),
-                  Text(
-                    _formatChange(data.change),
-                    style: TextStyle(color: changeColor),
-                  ),
-                ],
               ),
             ],
           ),
